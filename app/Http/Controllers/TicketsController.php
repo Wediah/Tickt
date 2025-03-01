ff67ef40b6bdf4e023de93f5b4c4afa57a609b51
Wediah <emmanuelwediah@gmail.com>
2025-03-01 23:53:12 +0000

    adds api response helpers on tickets model

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use F9Web\ApiResponseHelpers;
 use Illuminate\Http\Request;
 
 class TicketsController extends Controller
 {
-    //
+    use ApiResponseHelpers;
+
+    public function createTicket()
+    {
+
+    }
 }
